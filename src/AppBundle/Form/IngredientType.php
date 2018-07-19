c8a67963b3cb330666c10830c6ff1b609e414d02
romain coquery <coquery.romain@gmail.com>
2018-07-19 10:36:48 +0200

    add ingredient

--- src/AppBundle/Form/IngredientType.php
+++ src/AppBundle/Form/IngredientType.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class IngredientType extends AbstractType
 {
@@ -13,7 +15,11 @@ class IngredientType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('type');
+        $builder
+            ->add('type', TextType::class, [
+                'label' => 'Ingrédient *',
+                'constraints' => new NotBlank(['message' => 'Ce champs ne doit pas être vide']),
+            ]);
     }/**
      * {@inheritdoc}
      */
